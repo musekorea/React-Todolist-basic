0d4d52c491812582871397d61dd2382953cab0f9
musekorea <musekorea@hanmail.net>
2021-12-10 20:52:32 +0800

    complete

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import styles from './App.module.css';
+import React, { useState, useRef, useEffect } from 'react';
 
 function App() {
+  const [todo, setTodo] = useState('');
+  const [todos, setTodos] = useState([]);
+  const todoInput = useRef();
+  const inputTodo = (e) => {
+    setTodo((prev) => e.target.value);
+  };
+  const submitTodo = (e) => {
+    e.preventDefault();
+    if (todo === '') {
+      return;
+    }
+    setTodos((prev) => [todo, ...prev]);
+    setTodo('');
+  };
+  const delTodo = (e) => {
+    e.target.parentElement.remove();
+  };
+  useEffect(() => {
+    todoInput.current.focus();
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={styles.container}>
+      <h3>React Todo List</h3>
+      <ul>
+        {todos.map((todo, index) => (
+          <li key={index}>
+            {todo} <span onClick={delTodo}>🗑</span>{' '}
+          </li>
+        ))}
+      </ul>
+      <form action="" onSubmit={submitTodo}>
+        <input
+          type="text"
+          value={todo}
+          onChange={inputTodo}
+          ref={todoInput}
+          placeholder="Add a todo"
+        />
+      </form>
     </div>
   );
 }
